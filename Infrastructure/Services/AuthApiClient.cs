2a805a72b3e442e8bccc6fa6bcdb8a4c7ec4bb6e
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-03-08 20:39:41 -0500

    Se agrego la ruta para llamar al endpoint de modulo usuario

---
+++ Infrastructure/Services/AuthApiClient.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Model.Dto.Admin;
+using Infrastructure.Services.Interface;
+using System.Net.Http.Json;
+using Utilities.Shared;
+
+namespace Infrastructure.Services
+{
+    public class AuthApiClient : IAuthApiClient
+    {
+        private readonly HttpClient _client;
+
+        public AuthApiClient(IHttpClientFactory factory)
+        {
+            _client = factory.CreateClient("AuthApi");
+        }
+
+        public async Task<UsuarioAdmin?> ObtenerUsuarioAsync(int id) => await GetAsync<UsuarioAdmin>($"/auth/Usuario/{id}");
+    
+        private async Task<T?> GetAsync<T>(string url)
+        {
+            var response = await _client.GetAsync(url);
+            Console.WriteLine(response.StatusCode);
+
+            if (!response.IsSuccessStatusCode)
+                return default;
+
+            var apiResponse = await response.Content
+                .ReadFromJsonAsync<ApiResponse<T>>();
+
+            if (apiResponse is null || !apiResponse.IsSuccess)
+                return default;
+
+            return apiResponse.Data;
+        }
+    }
+}
