5b021ec2c301eddc2cd331318dcf0804a752baae
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-03-08 20:40:36 -0500

    Se agrego la ruta para llamar al endpoint del manejo de stock

---
+++ Infrastructure/Services/InventoryApiClient.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Model.Dto;
+using Infrastructure.Services.Interface;
+using System.Net.Http.Json;
+
+namespace Infrastructure.Services
+{
+    public class InventoryApiClient : IInventoryClient
+    {
+        private readonly HttpClient _client;
+
+        public InventoryApiClient(IHttpClientFactory factory)
+        {
+            _client = factory.CreateClient("InventoryApi");
+        }
+
+        public async Task<bool> RegistrarMovimientoAsync(ProductoMovimientoStock request)
+        {
+            var response = await _client.PostAsJsonAsync("/inventory/MovimientoStock", request);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
