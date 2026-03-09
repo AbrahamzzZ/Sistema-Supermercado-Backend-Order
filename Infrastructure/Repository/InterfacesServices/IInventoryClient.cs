80c588abb5e09a929e47118e10b6e3b7bd801932
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-03-08 20:50:54 -0500

    Se agrego la libreria Microsoft.AspNetCore.Http.Abstractions para un mejor manejo en las rutas hacia los endpoint de otros microservicios

--- Infrastructure/Repository/InterfacesServices/IInventoryClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Model.Dto;
-
-namespace Infrastructure.Repository.InterfacesServices
-{
-    public interface IInventoryClient
-    {
-        Task<bool> RegistrarMovimientoAsync(ProductoMovimientoStock request);
-    }
-}
