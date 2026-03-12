c337d91cd6bc8404c96b6c259cd586e649561c1e
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-03-11 23:05:35 -0500

    Se creo un nuevo dto para el detalle de la venta

---
+++ Domain/Model/Dto/Venta/DetalleVentaSpDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Model.Dto.Venta
+{
+    public class DetalleVentaSpDto
+    {
+        public int Id_Producto { get; set; }
+        public decimal Precio_Venta { get; set; }
+        public int Cantidad { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
